87c6836df7b8b905ad912945589e5cdef59d0e4d
mardiantodian04@gmail.com <mardiantodian04@gmail.com>
2023-11-09 14:22:04 +0800

    tambah introduction page dan login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
-latihan commit
+import 'package:flutter/material.dart';
+import 'package:kelompok4bc_pa_mobile/splash_screen.dart';
 
-sesuatu
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,75 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => StartState();
+}
+
+class StartState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = Duration(seconds: 4);
+    return Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return initWidget(context);
+  }
+
+  Widget initWidget(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xffF5591F),
+              gradient: LinearGradient(
+                colors: [Color(0xffF5591F), Color(0xffF2861E)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Image.asset("assets/images/logobg.png"),
+                ),
+                SizedBox(height: 20), // Jarak antara gambar dan teks
+                Text(
+                  "Welcome to App Laptop Recommendation",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
